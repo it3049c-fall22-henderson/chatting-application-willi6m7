14108fb3bce343642a64e589d3548b1deb659e69
willi6m7 <willi6m7@mail.uc.edu>
2022-09-25 14:36:54 -0400

    Backend Code

---
+++ resources/js/index.js
@@ -0,0 +1,76 @@
+const nameInput = document.getElementById("my-name-input");
+const myMessage = document.getElementById("my-message");
+const sendButton = document.getElementById("send-button");
+const chatBox = document.getElementById("chat");
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
+
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+async function updateMessages(){
+  const messages = await fetchMessages();
+  let formattedMessages = "";
+    messages.forEach(message => {
+        formattedMessages += formatMessage(message, nameInput.value);
+    });
+    chatBox.innerHTML = formattedMessages;
+}
+
+function fetchMessages() {
+  return fetch(serverURL)
+      .then( response => response.json())
+}
+
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
+
+function sendMessages(username, text) {
+  const newMessage = {
+      sender: username,
+      text: text,
+      timestamp: new Date()
+  }
+
+  fetch (serverURL, {
+      method: `POST`, 
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newMessage)
+  });
+}
+
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender,message);
+  myMessage.value = "";
+});
